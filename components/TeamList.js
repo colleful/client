64e2b29cbac839c9a232979f3501ba92f02f78e4
park sangbeom <smctkdqja@gmail.com>
2020-08-27 11:48:30 +0900

    style: Delete unnecessary code

--- components/TeamList.js
+++ components/TeamList.js
@@ -2,13 +2,7 @@ import React from 'react';
 
 import TeamListItem from './TeamListItem';
 const TeamList = ({team}) => {
-  return (
-    <>
-      {team.map((teams) => (
-        <TeamListItem key={teams.id} teams={teams} />
-      ))}
-    </>
-  );
+  return team.map((teams) => <TeamListItem key={teams.id} teams={teams} />);
 };
 
 export default TeamList;
